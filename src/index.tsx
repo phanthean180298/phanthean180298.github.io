5a7c348c8a347be146a3f7fe85c1ba76b4533248
QuangMinh <quangminhduong129@gmail.com>
2022-05-06 15:57:36 +0700

    add base object

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,21 @@ import {
   createStage,
   createViewport,
   createWhiteTexture,
+  loadFont,
+  Vector2
 } from "gdxjs";
 import GameState from "./GameState";
+import { BaseObject } from "./ui/baseObject";
 
 const WORLD_WIDTH = 600;
 const WORLD_HEIGHT = 1000;
+const INVENTORY_SLOT = 6;
+
+const ingredientSize = new Vector2(100, 100);
+const inventorySlotSize = new Vector2(80, 80);
 
 const init = async () => {
+
   const stage = createStage();
   const canvas = stage.getCanvas();
   const viewport = createViewport(canvas, WORLD_WIDTH, WORLD_HEIGHT);
@@ -22,12 +30,37 @@ const init = async () => {
   const batch = createBatch(gl);
   const gameState = new GameState();
 
+  let promise = loadFont(gl, "./assets/book-bold.fnt");
+  promise.then(font => {
+    console.log(font);
+  });
+
+  // Slot
+  let inventorySlots: BaseObject[] = [];
+  for(let i = 0; i < INVENTORY_SLOT; i++)
+  {
+    let slot = new BaseObject(gl);
+    slot.setSize(inventorySlotSize);
+    slot.setPosition(new Vector2(35 + i * (inventorySlotSize.x + 10), 500));
+    inventorySlots.push(slot);
+  }
+
   createGameLoop((delta: any) => {
     gameState.process(delta);
     batch.setProjection(camera.combined);
     batch.begin();
+    
+    // blacl panel
     batch.setColor(0, 0, 0, 1);
     batch.draw(whiteTexture, 0, 0, WORLD_WIDTH, WORLD_HEIGHT);
+    batch.setColor(1,1,1,1);
+    
+    // draw objs
+    for(let i = 0; i < inventorySlots.length; i++)
+    {
+      inventorySlots[i].draw(batch);
+    }
+
     batch.end();
   });
 };
